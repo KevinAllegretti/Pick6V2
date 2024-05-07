6796a008c5f7a212b5ee5256d0cf09bfc35e861f
KevinAllegretti <kevin.allegretti1@marist.edu>
2024-05-07 16:46:04 -0400

    increased timeout settings

--- src/microservices/connectDB.ts
+++ src/microservices/connectDB.ts
@@ -31,3 +31,9 @@ export async function connectToDatabase() {
   }
 }
 
+const mongoose = require('mongoose');
+const options = {
+  serverSelectionTimeoutMS: 30000, // Timeout after 30 seconds
+  socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
+};
+mongoose.connect('mongodb+srv://Kingbeats17:Yunglean17@pick6.nomxpzq.mongodb.net/', options);
