a3720d483eb4eb3defeaed191893d02426034b3f
KevinAllegretti <kevin.allegretti1@marist.edu>
2024-06-05 10:33:17 -0400

    button hover

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    apps: [
-      {
-        name: 'Pick6V2',
-        script: 'api/index.ts',
-        interpreter: 'ts-node',
-        env: {
-          NODE_ENV: 'development',
-          SENDGRID_API_KEY: process.env.SENDGRID_API_KEY // Ensure this matches your .env key
-        },
-        cwd: '/home/bitnami/htdocs/Pick6V2'
-      }
-    ]
-  };
-  
+  apps: [
+    {
+      name: 'Pick6V2',
+      script: 'api/index.ts',
+      interpreter: 'ts-node',
+      env: {
+        NODE_ENV: 'development',
+        SENDGRID_API_KEY: process.env.SENDGRID_API_KEY // Ensure this matches your .env key
+      },
+      cwd: '/home/bitnami/htdocs/Pick6V2'
+    }
+  ]
+};
