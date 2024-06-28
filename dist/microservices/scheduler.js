63a8dff8ca5c7957f1fd3795ed7ac490aa9feec0
KevinAllegretti <kevin.allegretti1@marist.edu>
2024-06-28 12:43:04 -0400

    timewindows1.3

--- dist/microservices/scheduler.js
+++ dist/microservices/scheduler.js
@@ -164,7 +164,7 @@ node_cron_1.default.schedule('0 19 * * 4', () => {
     (0, serverUtils_1.updateTuesdayStartTime)();
 });
 ////////
-node_cron_1.default.schedule('42 12 * * 5', () => {
+node_cron_1.default.schedule('44 12 * * 5', () => {
     console.log("It's Thursday 7:00 PM, now saving picks to last week");
     (0, serverUtils_1.savePicksToLastWeek)();
     console.log("Updating Tuesday start time to the upcoming Tuesday");
