91d44ee08ccc4c3cc8bef1aaf1c31e008dbe90f5
KevinAllegretti <kevin.allegretti1@marist.edu>
2024-10-10 15:36:41 -0400

    heap testuh oh

--- start.ts
+++ start.ts
@@ -3,12 +3,3 @@ import './api/index.ts';
 import './src/microservices/scheduler.ts';
 import './src/microservices/serverUtils.ts';
 
-setInterval(() => {
-    const memoryUsage = process.memoryUsage();
-    console.log(JSON.stringify({
-      rss: `${Math.round(memoryUsage.rss / 1024 / 1024 * 100) / 100} MB`,
-      heapTotal: `${Math.round(memoryUsage.heapTotal / 1024 / 1024 * 100) / 100} MB`,
-      heapUsed: `${Math.round(memoryUsage.heapUsed / 1024 / 1024 * 100) / 100} MB`,
-      external: `${Math.round(memoryUsage.external / 1024 / 1024 * 100) / 100} MB`
-    }));
-  }, 30000); // Log every 30 seconds
