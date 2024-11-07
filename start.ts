1a7b221e9c1acb8a90645eedb648018c6c93b12a
KevinAllegretti <kevin.allegretti1@marist.edu>
2024-11-07 10:44:24 -0500

    heap test

--- start.ts
+++ start.ts
@@ -2,4 +2,5 @@
 import './api/index.ts';
 import './src/microservices/scheduler.ts';
 import './src/microservices/serverUtils.ts';
-
+import * as v8 from 'v8';
+console.log('Heap Stats:', v8.getHeapStatistics());
