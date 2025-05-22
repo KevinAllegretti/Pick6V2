62b744febb166819bc90fe2ecd69763e8bcf5761
KevinAllegretti <kevin.allegretti1@marist.edu>
2025-05-22 14:33:00 -0400

    new golf pool styles

--- public/homepage.js
+++ public/homepage.js
@@ -6773,8 +6773,8 @@ async function displayGolfPoolContainer(pool) {
   // Pool name with golf icon
   const poolNameDiv = document.createElement("div");
   poolNameDiv.className = "golf-pool-name";
-  poolNameDiv.innerHTML = `<i class="fas fa-golf-ball"></i> ${pool.name}`;
-  
+  //poolNameDiv.innerHTML = `<i class="fas fa-golf-ball"></i> ${pool.name}`;
+    poolNameDiv.innerHTML = `${pool.name}`;
   headerTop.appendChild(poolNameDiv);
   
   // START DRAFT button - when in idle phase and user is admin
